aa0d4e2c5ec3d4f06faaa9b47a71107dbeaef7bf
Daryl Lee <daryllee@unm.edu>
2025-07-05 13:09:09 -0600

    Hello World working

---
+++ kernel/io.h
@@ -0,0 +1,7 @@
+#ifndef IO_H
+#define IO_H
+
+void uart_init(void);
+void uart_write_text(const char* buffer);
+
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,7 @@
+#include "io.h"
+
 void main(void) {
+    uart_init();
+    uart_write_text("Hello from RPi\n");
     while (1);
 }
